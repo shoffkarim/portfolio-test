045409d336b2980543c1519dfeb7184b0bc565e1
shoffkarim <k4r1mch1k@gmail.com>
2020-08-27 16:29:42 +0500

    js

--- waxom/prod/js/script.js
+++ waxom/prod/js/script.js
@@ -20,3 +20,37 @@ var swiper = new Swiper('.posts-slider', {
     prevEl: '.posts-button-prev',
   },
 });
+let tabs = document.querySelectorAll('.tabs__item');
+tabs.forEach(tab => tab.addEventListener('click', function(e){
+  e.preventDefault();
+  tabs.forEach(i => i.classList.remove('active'))
+  tab.classList.add('active');
+  let dataNum = tab.getAttribute('data-num-tab')
+  let projects = document.querySelectorAll('.projects-item');
+  projects.forEach(function(item) {
+    if(dataNum === "1") {
+      item.style.transition = "all 0.4s";
+      item.style.opacity = 0;
+      item.style.visibility = "visible";
+      setTimeout(() => {
+        item.style.display = "flex";
+        item.style.opacity = 1;
+      }, 400);
+    } else if(item.getAttribute('data-num-project') === dataNum){
+      item.style.transition = "all 0.4s";
+      item.style.opacity = 0;
+      item.style.visibility = "visible";
+      setTimeout(() => {
+        item.style.display = "flex";
+        item.style.opacity = 1;
+      }, 400);
+    } else {
+      item.style.transition = "all 0.4s";
+      item.style.opacity = 0;
+      item.style.visibility = "hidden";
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 400);
+    }
+  })
+}))
