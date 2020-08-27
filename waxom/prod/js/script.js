5a82a918e75a74368fd54b42378661cb197887ab
shoffkarim <k4r1mch1k@gmail.com>
2020-08-27 11:16:18 +0500

    posts, numbers, mobile

--- waxom/prod/js/script.js
+++ waxom/prod/js/script.js
@@ -10,3 +10,13 @@ var swiper = new Swiper('.slider', {
     prevEl: '.swiper-button-prev',
   },
 });
+var swiper = new Swiper('.posts-slider', {
+  speed: 600,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: '.posts-button-next',
+    prevEl: '.posts-button-prev',
+  },
+});
