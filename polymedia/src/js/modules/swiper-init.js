b9403b73082d0517b2d3c76956d6a8a4cf046777
shoffkarim <k4r1mch1k@gmail.com>
2020-09-14 12:56:27 +0500

    add section clients

--- polymedia/src/js/modules/swiper-init.js
+++ polymedia/src/js/modules/swiper-init.js
@@ -1,19 +1,35 @@
 /* eslint-disable no-unused-vars */
-import Swiper, { Navigation, Autoplay, Lazy } from 'swiper';
+import Swiper, { Navigation, Autoplay, Lazy } from "swiper";
 
 Swiper.use([Navigation, Autoplay, Lazy]);
 
-const headerSwiper = new Swiper('.header-slider', {
+const headerSwiper = new Swiper(".header-slider", {
   loop: true,
   lazy: {
-    loadPrevNext: true
+    loadPrevNext: true,
   },
   autoplay: {
     delay: 4500,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const aboutSwiper = new Swiper(".clients-container", {
+  autoHeight: true,
+  width: 1200,
+  centeredSlides: true,
+  initialSlide: 1,
+  centeredSlidesBounds: true,
+  lazy: {
+    loadPrevNext: true,
+  },
+  spaceBetween: 50,
+  navigation: {
+    nextEl: ".clients-slider__next",
+    prevEl: ".clients-slider__prev",
   },
 });
